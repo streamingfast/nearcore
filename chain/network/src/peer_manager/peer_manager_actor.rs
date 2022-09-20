f4918a4409c3636611cb9ddccbdd5617a1edc51a
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-09-20 17:05:16 -0400

    force connect to peers from environment variable PREFERRED_PEERS

--- chain/network/src/peer_manager/peer_manager_actor.rs
+++ chain/network/src/peer_manager/peer_manager_actor.rs
@@ -2,6 +2,7 @@ use crate::config;
 use crate::network_protocol::{AccountData, PeerMessage, RoutingTableUpdate, SyncAccountsData};
 use crate::peer_manager::connection;
 use crate::peer_manager::network_state::NetworkState;
+use crate::peer_manager::peer_store::get_preferred_peers;
 use crate::peer_manager::peer_store::PeerStore;
 use crate::private_actix::{
     PeerRequestResult, PeersRequest, RegisterPeer, RegisterPeerResponse, StopMsg, Unregister,
@@ -956,6 +957,13 @@ impl PeerManagerActor {
                 if !self.started_connect_attempts {
                     self.started_connect_attempts = true;
                     interval = default_interval;
+
+                    for preferred_peer in get_preferred_peers("PREFERRED_PEERS") {
+                        info!(target:"network", "Connecting to preferred peer {}", preferred_peer);
+                        ctx.notify(PeerManagerMessageRequest::OutboundTcpConnect(
+                            OutboundTcpConnect { peer_info: preferred_peer },
+                        ));
+                    }
                 }
 
                 ctx.notify(PeerManagerMessageRequest::OutboundTcpConnect(OutboundTcpConnect {
